9f87096f6bb26b2ee3ddef0de22c3f2b85159fb8
tayyaba muslim <muslimtayyaba@gmail.com>
2023-11-10 12:16:46 -0800

    feedback done

---
+++ admin/footer.php
@@ -0,0 +1,26 @@
+<div class="container-fluid mt-4 px-4" style="overflow: hidden;">
+    <div class="bg-secondary rounded-top p-4">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 text-center text-sm-start">
+                &copy; <a href="jeny.php">Jenny's Cosmetics</a>, All Right Reserved.
+            </div>
+            <div class="col-md-6 col-sm-6 text-center text-sm-end">
+                Designed By <a href="index.php">TST Web-devlopers/designer</a>
+                Distributed By: <a href="https://themewagon.com" target="_blank">Aptech</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="lib/chart/chart.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="lib/tempusdominus/js/moment.min.js"></script>
+<script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+<script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
